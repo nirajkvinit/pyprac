c602bdcdaa89f944a30f9e84cd315a3c03ce418f
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-03-31 22:21:31 +0530

    310320172221 timestamp

---
+++ hard_way/ex5.py
@@ -0,0 +1,14 @@
+my_name = 'Niraj Kumar'
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'dark'
+my_teeth = 'white'
+my_hair = 'dark'
+
+print "Let's talk about %s" % my_name
+print "He's %d inches tall" % my_height
+print "He's %d pounds heavy" % my_weight
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
