4a37c7089301ff6dec7c630c528e0190d44dbd49
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-03-31 23:59:40 +0530

    Updated

---
+++ ex1.py
@@ -0,0 +1,6 @@
+def printTwice(bruce) :
+	print(bruce)
+	print(bruce)
+
+
+printTwice('Niraj ' *4)
--- hard_way/ex5.py
+++ hard_way/ex5.py
@@ -6,9 +6,9 @@ my_eyes = 'dark'
 my_teeth = 'white'
 my_hair = 'dark'
 
-print "Let's talk about %s" % my_name
-print "He's %d inches tall" % my_height
-print "He's %d pounds heavy" % my_weight
-print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print ("Let's talk about %s" % my_name)
+print ("He's %d inches tall" % my_height)
+print ("He's %d pounds heavy" % my_weight)
+print ("He's got %s eyes and %s hair" % (my_eyes, my_hair))
 
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print ("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
