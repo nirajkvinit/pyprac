9a7f4a86b7b9d54d5984dbebbe514423e79b04d0
Niraj Kumar <nirajkvinit@gmail.com>
2018-08-24 18:05:17 +0530

    Updated

---
+++ hugtest.py
@@ -0,0 +1,10 @@
+import hug
+import webcolors
+
+@hug.get()
+def hextoname(hex: hug.types.text):
+    return webcolors.hex_to_name("#" + hex)
+
+@hug.get()
+def nametohex(name: hug.types.text):
+    return webcolors.name_to_hex(name)
